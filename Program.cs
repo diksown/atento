36447bfe4105649219f2936bb42c6e38851a8000
Dikson Santos <diksonfer@gmail.com>
2023-04-01 17:31:28 -0300

    chore: learning how to import libraries

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+	Console.WriteLine("case".ToQuantity(0));
+	Console.WriteLine("case".ToQuantity(1));
+	Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+	Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+	Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+	Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+	Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
