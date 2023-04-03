a3782dcfa30b703cb7ab6558fa74f6b87ba2f012
Dikson Santos <diksonfer@gmail.com>
2023-04-03 00:46:25 -0300

    feat: add email templates

--- Email.cs
+++ Email.cs
@@ -11,21 +11,40 @@ namespace Atento
 			Buy, Sell
 		}
 
-		private static string emailBodyTemplate(StockActionType emailType, string stockSymbol, decimal stockPrice)
+		private static string fmtStock(decimal stockDecimal)
 		{
-			return "not implemented";
+			return string.Format("{0:0.00}", stockDecimal);
 		}
 
-		private static string emailTitleTemplate(StockActionType emailType, string stockSymbol)
+		private static string emailBodyTemplate(StockActionType emailType, string stockSymbol, decimal curStockPrice, decimal priceLimit)
 		{
-			return "not implemented";
+			bool isBuying = emailType == StockActionType.Buy;
+			string emailBody = "Olá!\n\n" +
+			$"O valor das ações de {stockSymbol} está R${fmtStock(curStockPrice)}, " +
+			$"{(isBuying ? "abaixo" : "acima")} do limite de R${fmtStock(priceLimit)} estabelecido por você. " +
+			$"{(isBuying ? "Compre" : "Venda")} agora!\n\n" +
+			"Atenciosamente,\n\n" +
+			"Equipe atento.";
+			return emailBody;
 		}
 
-		public static void MockSendEmail(StockActionType emailType, string stockSymbol, decimal stockPrice)
+		private static string emailTitleTemplate(StockActionType emailType, string stockSymbol, decimal curStockPrice)
 		{
-			string emailBody = emailBodyTemplate(emailType, stockSymbol, stockPrice);
-			string emailTitle = emailTitleTemplate(emailType, stockSymbol);
+			bool isBuying = emailType == StockActionType.Buy;
+			string emailTitle = $"{(isBuying ? "Compre" : "Venda")} " +
+			$"ações de {stockSymbol} por R${fmtStock(curStockPrice)}";
+			return emailTitle;
+		}
 
+		public static void MockSendEmail(StockActionType emailType, string stockSymbol, decimal curStockPrice, decimal priceLimit)
+		{
+			string emailTitle = emailTitleTemplate(emailType, stockSymbol, curStockPrice);
+			string emailBody = emailBodyTemplate(emailType, stockSymbol, curStockPrice, priceLimit);
+
+			Console.WriteLine(emailTitle);
+			Console.WriteLine("");
+			Console.WriteLine(emailBody);
+			Console.WriteLine("==============");
 		}
 	}
 }
--- StockWorker.cs
+++ StockWorker.cs
@@ -2,28 +2,51 @@ namespace Atento
 {
 	class Worker
 	{
+		private static int TimeBetweenApiCalls = 1000;
+
 		public static async Task watchStock(string stockSymbol, decimal priceToSell, decimal priceToBuy)
 		{
+			// These flags will be used to avoid sending multiple
+			// emails suggesting the same thing
+			bool shouldSendSellEmail = true;
+			bool shouldSendBuyEmail = true;
+			decimal stockPrice = 0;
+
 			while (true)
 			{
-				decimal stockPrice = await ApiWrapper.MockGetStockPrice(stockSymbol);
+				stockPrice = await ApiWrapper.MockGetStockPrice(stockSymbol);
 
-				// These flags will be used to avoid sending multiple
-				// emails suggesting the same thing
-				bool shouldSendSellEmail = true;
-				bool shouldSendBuyEmail = true;
+				System.Console.WriteLine("Price is " + stockPrice);
+
 				if (stockPrice > priceToSell && shouldSendSellEmail)
 				{
-					Email.MockSendEmail(Email.StockActionType.Buy, stockSymbol, stockPrice);
-					shouldSendBuyEmail = true;
+					Email.MockSendEmail(
+						Email.StockActionType.Sell,
+						stockSymbol,
+						stockPrice,
+						priceToSell
+					);
 					shouldSendSellEmail = false;
 				}
+
 				if (stockPrice < priceToBuy && shouldSendBuyEmail)
 				{
-					Email.MockSendEmail(Email.StockActionType.Sell, stockSymbol, stockPrice);
-					shouldSendSellEmail = true;
+					Email.MockSendEmail(
+						Email.StockActionType.Buy,
+						stockSymbol,
+						stockPrice,
+						priceToBuy
+					);
 					shouldSendBuyEmail = false;
 				}
+
+				if (stockPrice <= priceToSell)
+					shouldSendSellEmail = true;
+
+				if (stockPrice >= priceToBuy)
+					shouldSendBuyEmail = true;
+
+				await Task.Delay(TimeBetweenApiCalls);
 			}
 		}
 	}
